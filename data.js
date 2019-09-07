033ddf1925cd2332c7a4f75d9dc81ead59954dac
Sai Iyengar <saiviyengar@gmail.com>
2019-09-07 00:22:32 -0700

    Jukebox Vol1

---
+++ data.js
@@ -0,0 +1,189 @@
+/**
+ * Data for each alphabet with the sound, color and shape attribute
+ */
+
+
+var data = {
+	q: {
+		sound: new Howl({
+			src: ['assets/sounds/bubbles.mp3']
+		}),
+		color: '#1abc9c',
+		shape: 'circle'
+	},
+	w: {
+		sound: new Howl({
+			src: ['assets/sounds/clay.mp3']
+		}),
+		color: '#2ecc71',
+		shape: 'octagon'
+	},
+	e: {
+		sound: new Howl({
+			src: ['assets/sounds/confetti.mp3']
+		}),
+		color: '#3498db',
+		shape: 'rectangle'
+	},
+	r: {
+		sound: new Howl({
+			src: ['assets/sounds/corona.mp3']
+		}),
+		color: '#9b59b6',
+		shape: 'rectangle'
+	},
+	t: {
+		sound: new Howl({
+			src: ['assets/sounds/dotted-spiral.mp3']
+		}),
+		color: '#34495e',
+		shape: 'octagon'
+	},
+	y: {
+		sound: new Howl({
+			src: ['assets/sounds/flash-1.mp3']
+		}),
+		color: '#16a085',
+		shape: 'circle'
+	},
+	u: {
+		sound: new Howl({
+			src: ['assets/sounds/flash-2.mp3']
+		}),
+		color: '#27ae60',
+		shape: 'octagon'
+	},
+	i: {
+		sound: new Howl({
+			src: ['assets/sounds/flash-3.mp3']
+		}),
+		color: '#2980b9',
+		shape: 'octagon'
+	},
+	o: {
+		sound: new Howl({
+			src: ['assets/sounds/glimmer.mp3']
+		}),
+		color: '#8e44ad',
+		shape: 'triangle'
+	},
+	p: {
+		sound: new Howl({
+			src: ['assets/sounds/moon.mp3']
+		}),
+		color: '#2c3e50',
+		shape: 'octagon'
+	},
+	a: {
+		sound: new Howl({
+			src: ['assets/sounds/pinwheel.mp3']
+		}),
+		color: '#f1c40f',
+		shape: 'rectangle'
+	},
+	s: {
+		sound: new Howl({
+			src: ['assets/sounds/piston-1.mp3']
+		}),
+		color: '#e67e22',
+		shape: 'rectangle'
+	},
+	d: {
+		sound: new Howl({
+			src: ['assets/sounds/piston-2.mp3']
+		}),
+		color: '#e74c3c',
+		shape: 'circle'
+	},
+	f: {
+		sound: new Howl({
+			src: ['assets/sounds/prism-1.mp3']
+		}),
+		color: '#95a5a6',
+		shape: 'triangle'
+	},
+	g: {
+		sound: new Howl({
+			src: ['assets/sounds/prism-2.mp3']
+		}),
+		color: '#f39c12',
+		shape: 'rectangle'
+	},
+	h: {
+		sound: new Howl({
+			src: ['assets/sounds/prism-3.mp3']
+		}),
+		color: '#d35400',
+		shape: 'rectangle'
+	},
+	j: {
+		sound: new Howl({
+			src: ['assets/sounds/splits.mp3']
+		}),
+		color: '#1abc9c',
+		shape: 'circle'
+	},
+	k: {
+		sound: new Howl({
+			src: ['assets/sounds/squiggle.mp3']
+		}),
+		color: '#2ecc71',
+		shape: 'triangle'
+	},
+	l: {
+		sound: new Howl({
+			src: ['assets/sounds/strike.mp3']
+		}),
+		color: '#3498db',
+		shape: 'circle'
+	},
+	z: {
+		sound: new Howl({
+			src: ['assets/sounds/suspension.mp3']
+		}),
+		color: '#9b59b6',
+		shape: 'triangle'
+	},
+	x: {
+		sound: new Howl({
+			src: ['assets/sounds/timer.mp3']
+		}),
+		color: '#34495e',
+		shape: 'circle'
+	},
+	c: {
+		sound: new Howl({
+			src: ['assets/sounds/ufo.mp3']
+		}),
+		color: '#16a085',
+		shape: 'octagon'
+	},
+	v: {
+		sound: new Howl({
+			src: ['assets/sounds/veil.mp3']
+		}),
+		color: '#27ae60',
+		shape: 'triangle'
+	},
+	b: {
+		sound: new Howl({
+			src: ['assets/sounds/wipe.mp3']
+		}),
+		color: '#2980b9',
+		shape: 'circle'
+	},
+	n: {
+		sound: new Howl({
+			src: ['assets/sounds/zig-zag.mp3']
+		}),
+		color: '#8e44ad',
+		shape: 'triangle'
+	},
+	m: {
+		sound: new Howl({
+			src: ['assets/sounds/moon.mp3']
+		}),
+		color: '#2c3e50',
+		shape: 'rectangle'
+	}
+}
